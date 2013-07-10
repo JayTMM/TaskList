cfd3300f2a757c6092ce914c33451b31e5be19bf
Jay O <Jay@TooMuchMetal.net>
2013-07-10 14:46:41 -0400

    Week 4 Update (See extended description)
    + Added clearFinishedTasks(); which can be fired from the console.
    
    + Added addTask(taskName, dueDate, taskComplete) that can be fired from
    the console.
    + taskName is a string
    + dueDate is a string in the format of yyyy, mm, dd
    + taskComplete is a boolean
    + addTask fires off a small set of functions that refresh the list with
    the newest task at the bottom
    
    + printTask() can be fired on it's own from the console, if you really
    wanted to duplicate your list for no reason. Maybe you're just a
    masochist.
    
    + uses strict mode now, if only to get JSHint to shut up. Several of the
    things it complained about "===" instead of "==" and my use of
    semicolons at the end of functions were really semantic stuff. I fixed
    the "===" so that strict mode worked better, but I left my semicolons
    because they are merely a dialect choice.
    +Use of strict mode requires defining throwaway variables for running
    for loops, which seems silly and adds more work.
    
    + Space Marine Gauss Rifle now does + 4 damage per shot.

---
+++ TaskList_Week4/js/taskList_LINTY.js
@@ -0,0 +1,132 @@
+var today = new Date();
+
+var close = today.getDate() + 6;
+
+
+
+
+var taskList = [
+{
+"taskName": "Realize I am not in Kansas anymore",
+"dueDate": new Date(2013,5,15),
+"taskComplete": true
+},
+{
+"taskName": "Enjoy all that the 1960s has to offer",
+"dueDate": new Date(2013,5,15),
+"taskComplete": true
+},
+{
+"taskName": "See what this Pantone fuss is all about",
+"dueDate": new Date(2013,6,9),
+"taskComplete": false
+},
+{
+"taskName": "Pick up Milk and Bread at the store",
+"dueDate": new Date(2013,6,12),
+"taskComplete": false
+},
+{
+"taskName": "See a University of Hawai'i football game",
+"dueDate": new Date(2013,6,26),
+"taskComplete": false
+},
+{
+"taskName": "Whistle The Launch Codes to Start World War III!",
+"dueDate": new Date(1989,8,1),
+"taskComplete": true
+}
+];
+
+function printTask() {
+
+	var today = new Date();
+	
+	var close = today.getDate() + 6;
+
+	for (t=0; t < taskList.length; t++) {
+
+
+		if (taskList[t].taskComplete == true) {
+	
+				$('<li class="complete">' + '<div class="greenBlock"></div>' + '<p>' + taskList[t].taskName + '</p>' + '</li>').appendTo('#taskListInner');
+	
+			} else if ((taskList[t].taskComplete == false) && (today > taskList[t].dueDate)) {
+	
+				$('<li class="incomplete">' + '<div class="redBlock"></div>' + '<p>' + taskList[t].taskName + '</p>' + '</li>').appendTo('#taskListInner');
+				
+			} else if ((taskList[t].taskComplete == false) && (close > taskList[t].dueDate.getDate())) {
+	
+				$('<li class="incomplete">' + '<div class="yellowBlock"></div>' + '<p>' + taskList[t].taskName + '</p>' + '</li>').appendTo('#taskListInner');
+	
+			} else if ((taskList[t].taskComplete == false) && (today < taskList[t].dueDate)) {
+	
+				$('<li class="incomplete">' + '<div class="greyBlock"></div>' + '<p>' + taskList[t].taskName + '</p>' + '</li>').appendTo('#taskListInner');
+				
+
+	
+		}
+
+	}
+	
+
+};
+
+function clearFinishedTasks() {
+
+	$('.complete').fadeOut();
+	
+	return "I cleaned that list up, hoss!";
+
+};
+
+function addTask(taskString, taskDueDate, taskStatus) {
+
+taskList.push({taskName: taskString, dueDate: new Date(taskDueDate), taskComplete: taskStatus})
+
+$('.complete').fadeOut();
+
+$('.incomplete').fadeOut();
+
+printTask();
+
+return "I rebooted that there task list, hoss!"
+
+};
+
+$('<ul>').appendTo('#taskListInner');
+
+printTask();
+
+$('</ul>').appendTo('#taskListInner');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
